77d580f7d90afd5341c90d094222fd049bc26fba
Syed Mohammad Ibrahim <iamibi@umd.edu>
2021-09-25 13:11:49 -0400

    Implemented a fix for Reflected Xss

--- WebGoat/Content/ReflectedXSS.aspx.cs
+++ WebGoat/Content/ReflectedXSS.aspx.cs
@@ -7,6 +7,7 @@ using System.Web.UI.WebControls;
 using System.Data;
 using OWASP.WebGoat.NET.App_Code.DB;
 using OWASP.WebGoat.NET.App_Code;
+using System.Web.Security.AntiXss;
 
 namespace OWASP.WebGoat.NET
 {
@@ -22,8 +23,19 @@ namespace OWASP.WebGoat.NET
 
 		void LoadCity (String city)
 		{
+            // Whitelist for the cities.
+            string[] cities = { "San Francisco", "Boston", "NYC", "Paris", "Tokyo", "Sydney", "London" };
+
+            // Return early if the city is not present in the whitelist.
+            if (!cities.Contains(city))
+            {
+                lblOutput.Text = "Invalid city";
+                dtlView.DataSource = null;
+                dtlView.DataBind();
+                return;
+            }
             DataSet ds = du.GetOffice(city);
-            lblOutput.Text = "Here are the details for our " + city + " Office";
+            lblOutput.Text = $"Here are the details for our {AntiXssEncoder.HtmlEncode(city, false)} Office";
             dtlView.DataSource = ds.Tables[0];
             dtlView.DataBind();
 		}
