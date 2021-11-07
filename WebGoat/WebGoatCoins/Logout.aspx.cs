3b191ee7240c36ac00d7b215cd76d4693238a0ff
Syed Mohammad Ibrahim <iamibi@umd.edu>
2021-11-07 13:34:30 -0500

    Implemented fix for Lab 8 Phase 1

--- WebGoat/WebGoatCoins/Logout.aspx.cs
+++ WebGoat/WebGoatCoins/Logout.aspx.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -21,6 +22,13 @@ namespace OWASP.WebGoat.NET.WebGoatCoins
             Session.Clear();
             Session.Abandon();
             Session.RemoveAll();
+
+            if (Request.Cookies["ASP.NET_SessionId"] != null)
+            {
+                Response.Cookies["ASP.NET_SessionId"].Value = string.Empty;
+                Response.Cookies["ASP.NET_SessionId"].Expires = DateTime.Now.AddMonths(-20);
+            }
+
             Response.Redirect("/Default.aspx");
         }
     }
